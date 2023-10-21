2fbb6dcda28ffd6361fe6c7990cb64fa90138a8e
Koroko <brycemckever8@gmail.com>
2023-10-21 18:26:34 -0400

    adds basic functionailty for molten tools all of them to sword axe shovel pickaxe no hoe i dont see the point in that unless its something special.

--- src/main/java/turniplabs/examplemod/ModItems.java
+++ src/main/java/turniplabs/examplemod/ModItems.java
@@ -13,6 +13,12 @@ public class ModItems {
 		(new ToolMaterial()).setDurability(100).setEfficiency(8.0F, 43.0F).setMiningLevel(2);
 	public static final Item swordMolten = ItemHelper.createItem(ExampleMod.MOD_ID, new ItemToolSword("sword.molten", Block.blocksList.length + 3000, molten), "sword.molten", "swordMolten.png").setMaxStackSize(1).withTags(ItemTags.preventCreativeMining);
 
+	public static final Item pickaxemolten = ItemHelper.createItem(ExampleMod.MOD_ID, new ItemToolPickaxe("Pickaxe.molten", Block.blocksList.length + 3001, molten), "Pickaxe.molten", "PickaxeMolten.png").setMaxStackSize(1).withTags(ItemTags.preventCreativeMining);
+
+	public static final Item axemolten = ItemHelper.createItem(ExampleMod.MOD_ID, new ItemToolAxe("Axe.molten", Block.blocksList.length + 3002, molten), "Axe.molten", "MoltenAxe.png").setMaxStackSize(1).withTags(ItemTags.preventCreativeMining);
+
+	public static final Item shovelmolten = ItemHelper.createItem(ExampleMod.MOD_ID, new ItemToolShovel("shovel.molten", Block.blocksList.length + 3003, molten), "shovel.molten", "MoltenShovel.png").setMaxStackSize(1).withTags(ItemTags.preventCreativeMining);
+
 
 	public static void register(){}
 
